7b7a828aebdfe3a8060bf9a629cbff162f70a07c
nurielo <83958112+nurielo@users.noreply.github.com>
2021-09-29 12:04:21 +0300

    update for task 7

---
+++ orin_nuriel_app/DB/db.config.js
@@ -0,0 +1,6 @@
+module.exports={
+    HOST:"localhost",
+    USER:"root",
+    PASSWORD:"nurielO26",
+    DB: "mysql"
+};
--- orin_nuriel_app/app.js
+++ orin_nuriel_app/app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+//html route
+app.get("/cv",(req,res)=>{
+  res.sendFile(path.join(__dirname,"./views"))
+});
+
+app.get("/myFirstHtml",(req,res)=>{
+  res.sendFile(path.join(__dirname,"./views"))
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -39,3 +49,38 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+const sql= require('./db');
+const express=require('express');
+const bodyParser = require("body-parser");
+const app=express();
+
+// parse requests of contenttype: application/json
+app.use(bodyParser.json());
+
+// parse requests of contenttype: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true}));
+// simple route
+app.get("/", (req, res) => {
+res.json({ message: "Welcome to web course example application." });
+});
+
+//get contact 
+app.get("/contact",function(req,res){
+    sql.query("select * from contact",(err,mysqlres)=>{
+        if (err) {
+            console.log("error: ", err);
+            res.status(400).send({message: "error in getting all contact: " + err});return;
+            }
+            console.log("got all contact...");
+            res.send(mysqlres);
+            return;
+            });
+            });
+
+
+// set port, listen for requests
+app.listen(3000, () => {
+console.log("Server is running on port 3000."
+);
+});
